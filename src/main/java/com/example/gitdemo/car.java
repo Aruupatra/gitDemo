6e226ce88ae63fac87ccc36d1952cd0a8183610f
Arun <parun1331@gmail.com>
2024-05-16 08:58:08 +0530

    car commited

---
+++ src/main/java/com/example/gitdemo/car.java
@@ -0,0 +1,12 @@
+package com.example.gitdemo;
+
+public class car {
+
+    String name;
+    int number;
+    public  car(String name,int number)
+    {
+        this.name=name;
+        this.number=number;
+    }
+}
